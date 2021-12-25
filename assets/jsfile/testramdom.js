260d27befe8c6b97715f2f817b7d1bd2e3f6ec5d
Tungnv <ttung2k23@gmail.com>
2021-12-25 13:11:08 +0700

    demo 3

--- assets/jsfile/testramdom.js
+++ assets/jsfile/testramdom.js
@@ -1,6 +1,11 @@
 const btnStt = document.querySelector(".button-tow");
 const mp3Audio= document.getElementById("playAudio");
 
+window.onload =  function(){
+   mp3Audio.load();
+   mp3Audio.play();
+}
+
 function dispy(){
    const  valueas = document.querySelector(".form-control").value;
    
@@ -12,8 +17,6 @@ function dispy(){
       mp3Audio.innerHTML= `<source src="./assets/audio_file/nhaccc.mp3">`
       mp3Audio.load();
       mp3Audio.play();
-  
-      
    }
 }
 function display2(){
